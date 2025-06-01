c9629a12ed231b592610a85c9a14c9d924a42287
Xi LIU <xi.liu@u.nus.edu>
2025-06-01 15:06:39 +0800

    plt

--- resnet_no_bn/README.md
+++ resnet_no_bn/README.md
@@ -5,3 +5,14 @@
 For final q=sum, use neural network structure in no_bn_resnet_structure_final_sum.py
 1.  training:
     python no_bn_resnet_final_sum_train.py L r num_layers num_neurons
+2. plot training log:
+    in plt_log.py, change num_layers, then
+    python plt_log.py
+3. performance on test set: 
+    in no_bn_resnet_final_sum_test.py, change num_layers, then
+    python no_bn_resnet_final_sum_test.py
+4. extract test set performance results for 1 num_layers, all num_neurons:
+    in extract_test.py, change num_layers, then
+    python extract_test.py
+5. plot loss:
+    python plt_std_loss.py
---
+++ resnet_no_bn/extract_test.py
@@ -0,0 +1,58 @@
+import numpy as np
+import re
+import matplotlib.pyplot as plt
+from pathlib import Path
+import pandas as pd
+from itertools import combinations
+import glob
+
+#this script combines test results for the same num_layers, all num_neurons
+
+
+L = 15  # Number of spins
+r = 3   # Number of spins in each interaction term
+B = list(combinations(range(L), r))
+K=len(B)
+num_layers=3
+
+num_neurons_vec=[]
+num_neurons_dirs_vec=[]
+dirs_root=f"./out_model_resnet_final_sum_L{L}_K{K}_r{r}_layer{num_layers}/"
+
+for dir in glob.glob(dirs_root+f"/neuron*"):
+    match_neuron_num=re.search(r"neuron(\d+)", dir)
+    if match_neuron_num:
+        num_neurons_dirs_vec.append(dir)
+        num_neurons_vec.append(int(match_neuron_num.group(1)))
+
+sorted_inds=np.argsort(num_neurons_vec)
+sorted_num_neurons_vec=[num_neurons_vec[ind] for ind in sorted_inds]
+sorted_num_neurons_dirs_vec=[num_neurons_dirs_vec[ind] for ind in sorted_inds]
+pattern_std=r'std_loss\s*=\s*([+]?(?:\d*\.\d+|\d+)(?:[eE][-+]?\d+)?)'
+
+
+def load_one_test_set_result(dir):
+    test_result_file=dir+"/test.txt"
+    with open(test_result_file,"r") as fptr:
+        line=fptr.readline()
+    match_std_loss = re.search(pattern_std, line)
+    if match_std_loss:
+        return float(match_std_loss.group(1))
+    else:
+        print(f"{test_result_file}, format error")
+        exit(12)
+
+outPath="./compare_layer_neuron_num/"
+Path(outPath).mkdir(parents=True, exist_ok=True)
+std_loss_vec=[]
+for ind in range(0,len(sorted_num_neurons_dirs_vec)):
+    num_neurons=sorted_num_neurons_vec[ind]
+    dir=sorted_num_neurons_dirs_vec[ind]
+    std_loss=load_one_test_set_result(dir)
+    std_loss_vec.append(std_loss)
+
+out_df=pd.DataFrame({
+"neuron_num":sorted_num_neurons_vec,
+    "std_loss":std_loss_vec
+})
+out_df.to_csv(outPath+f"layer{num_layers}_std_loss.csv",index=False)
